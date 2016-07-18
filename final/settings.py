907b53d2d371f5d5e30788e4133cfac0624be7f1
kaptsov <kaptsovaa@gmail.com>
2016-07-19 01:14:12 +0600

    Update settings.py

--- final/settings.py
+++ final/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'fipi_db',
         'USER': 'postgres',
-        'PASSWORD': 'jgwssljcnegf784523',
+        'PASSWORD': '111',
         'HOST': 'localhost',
         'PORT': '5432',
     }
